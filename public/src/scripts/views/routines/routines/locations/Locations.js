454f57fb69675c766bf70cf69b1cb43e737e5a0a
Daniel Alba <albad869@gmail.com>
2024-02-23 10:10:59 -0500

    changes

--- public/src/scripts/views/routines/routines/locations/Locations.js
+++ public/src/scripts/views/routines/routines/locations/Locations.js
@@ -361,6 +361,16 @@ export class Locations {
                 //console.log(latitud)
                 const longitud = parseFloat(coords[1].trim());
                 //console.log(longitud)
+                const timeIni = inputsCollection.scheduleTime.value.split(':');
+                const hourIni = parseInt(timeIni[0].trim());
+                //console.log(hourIni)
+                const minIni = parseInt(timeIni[1].trim());
+                //console.log(minIni)
+                const timeEnd = inputsCollection.scheduleTimeEnd.value.split(':');
+                const hourEnd = parseInt(timeEnd[0].trim());
+                //console.log(hourEnd)
+                const minEnd = parseInt(timeEnd[1].trim());
+                //console.log(minEnd)
                 const raw = JSON.stringify({
                     "name": `${inputsCollection.name.value}`,
                     "cords": `${inputsCollection.cords.value}`,
@@ -390,6 +400,8 @@ export class Locations {
                   alert("Distancia inválida");
                 }else if(routine.id == '' || routine.id == null || routine.id == undefined){
                   alert("No hay rutina");
+                }else if(hourIni == hourEnd && minIni > minEnd){
+                  alert("Minutos iniciales no pueden ser mayores a las del final en horas iguales.");
                 }else{
                   registerEntity(raw, 'RoutineSchedule');
                   setTimeout(() => {
@@ -624,6 +636,16 @@ export class Locations {
             //console.log(latitud)
             const longitud = parseFloat(coords[1].trim());
             //console.log(longitud)
+            const timeIni = $value.scheduleTime.value.split(':');
+            const hourIni = parseInt(timeIni[0].trim());
+            //console.log(hourIni)
+            const minIni = parseInt(timeIni[1].trim());
+            //console.log(minIni)
+            const timeEnd = $value.scheduleTimeEnd.value.split(':');
+            const hourEnd = parseInt(timeEnd[0].trim());
+            //console.log(hourEnd)
+            const minEnd = parseInt(timeEnd[1].trim());
+            //console.log(minEnd)
             let raw = JSON.stringify({
                 // @ts-ignore
                 "name": `${$value.name.value}`,
@@ -643,6 +665,8 @@ export class Locations {
               alert("No se ha seleccionado una ubicación");
             }else if($value.distance.value == "" || $value.distance.value == undefined || $value.distance.value < 0){
               alert("Distancia inválida");
+            }else if(hourIni == hourEnd && minIni > minEnd){
+              alert("Minutos iniciales no pueden ser mayores a las del final en horas iguales.");
             }else{
               update(raw);
             }
